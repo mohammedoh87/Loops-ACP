253d55653037755d5f339160657605101a5d8cbf
mohammedoh87 <aamina.oh87@gmail.com>
2024-09-06 10:42:25 +0300

    Loops ACP Correction

--- Power Calculator.py
+++ Power Calculator.py
@@ -1,5 +1,7 @@
 n = int(input("Enter a number: "))
+exponent = int(input("Enter the power: "))
 
-for i in range(1,n+1):
-    n = n ** i
-    print(n)
+
+for i in range(1,exponent+1):
+    product = n ** i
+    print(product)
